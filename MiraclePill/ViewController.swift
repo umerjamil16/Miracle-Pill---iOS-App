2946c3fdd1a167673f9c4ce467622a27c37aedf5
Umer <umerjamil@protonmail.com>
2017-05-07 22:18:28 +0500

    Initial Commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Umer on 07/05/2017.
+//  Copyright © 2017 Umer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
